3d2a3c9c3488fc95cdd72ea4f944f86c3c88b931
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-02 05:41:42 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootdockerbuildpacks/SpringbootDockerBuildpacksApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootdockerbuildpacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerBuildpacksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDockerBuildpacksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootdockerbuildpacks/SpringbootDockerBuildpacksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootdockerbuildpacks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerBuildpacksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
